66dfafbdb2513e279f3e97adda90b7d1fcf6f725
JLJOJO74 <99445781+JLJOJO74@users.noreply.github.com>
2022-02-11 16:43:39 -0500

    Update variables.php
    changed company name to LLC

--- variables.php
+++ variables.php
@@ -3,7 +3,7 @@
 			  echo "<br/>";
 			
 			// include function
-			$company_name = "Guitar World, Inc.";
+			$company_name = "Guitar World, LLC";
 			$tagline = "Teaching Guitar since 2001";
 			
 			
